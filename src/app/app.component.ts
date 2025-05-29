7bc10574223d864f17c27c017a81a3d73380847c
edivaldo smith <edivaldosmith@gmail.com>
2025-05-29 16:00:44 +0100

    Fiz a tela inicial, a tela de login e a tela de cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './view/login/login.component';
+import { CadastroComponent } from './view/cadastro/cadastro.component';
+import { TelaInicialComponent } from './view/tela-inicial/tela-inicial.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ FormsModule, RouterOutlet, RouterLink, LoginComponent,CadastroComponent,TelaInicialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angular';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  
 }
+
+  
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './view/tela-inicial/tela-inicial.component';
+import { LoginComponent } from './view/login/login.component';
+import { CadastroComponent } from './view/cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "tela", component:TelaInicialComponent     
+    },
+
+    {
+        path: "login", component:LoginComponent
+    },
+    {
+        path: "cadastro", component:CadastroComponent
+    }
+
+];
